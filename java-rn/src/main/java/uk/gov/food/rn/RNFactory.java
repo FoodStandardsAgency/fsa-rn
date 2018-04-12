e7245c853fbc3808eea57204eaad8bffef345927
Ian Dickinson <i.j.dickinson@gmail.com>
2018-04-12 22:52:01 +0100

    Refactoring of RN.java to use the new Representation class for the encoding and decoding of the external forms. Also:
    * re-ordered the methods to put constructors at the top of the file
    * simplified parsing of decimals using a Regexp
    * simplified hash code generation
    * improved test coverage
    
    Conceivably, parseDecimal() and packElements() should move to the
    Representation class, but that's a job for another refactoring.

--- java-rn/src/main/java/uk/gov/food/rn/RNFactory.java
+++ java-rn/src/main/java/uk/gov/food/rn/RNFactory.java
@@ -2,13 +2,12 @@
  * File:        RNFactory.java
  * Created by:  Stuart Williams (skw@epimorphics.com)
  * Created on:  3 Apr 2018
- * 
+ *
  * Copyright (c) 2018 Crown Copyright (Food Standards Agency)
- * 
+ *
  *****************************************************************/
 package uk.gov.food.rn;
 
-import java.math.BigInteger;
 import java.time.Instant;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
@@ -16,127 +15,127 @@ import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 public class RNFactory  {
-	private final Authority authority  ;
-	private final Instance  instance   ;
-	private final Type      type       ;
-	private long  prev = 0 ;
-	
-	
-	public boolean equals(RNFactory other) {
-		return authority.equals(other.authority) &&
+    private final Authority authority  ;
+    private final Instance  instance   ;
+    private final Type      type       ;
+    private long  prev = 0 ;
+
+
+    public boolean equals(RNFactory other) {
+        return authority.equals(other.authority) &&
                instance.equals(other.instance) &&
                type.equals(other.type);
-	}
-	
-	@Override
-	public int hashCode() {
-		int res = authority.getId();		
-		res     = instance.getId() + res*(Instance.MAX_INSTANCE_ID+1);
-		res     = type.getId()     + res*(Type.MAX_TYPE_ID+1);	
-		return res;
-	}
-	
-	/*
-	 *  Use a Map to ensure there is a single factory instance for each authority,instance and type combination
-	 *  within the same JVM.
-	 *  
-	 */
-	
-	private static HashMap<Integer,RNFactory> factories = new HashMap<Integer,RNFactory>() ;
-	
-	public static RNFactory getFactory(Authority authority, Instance instance, Type type) throws RNException {
-		RNFactory res = new RNFactory(authority, instance, type);
-		
-		synchronized (factories) {
-			if (factories.containsKey(res.hashCode())) {
-				res = factories.get(res.hashCode());
-			} else {
-				factories.put(res.hashCode(), res);
-			}
-		}
-		
-		return res;
-	}	
-	
-	private RNFactory(Authority authority, Instance instance, Type type) {
-		this.authority = authority;
-		this.instance  = instance;
-		this.type      = type;
-	}
-	
-	public RN generateReferenceNumber() {
-		// Make sure at least one millisecond has elapse since the last reference number was generated
-		long time = 0;
-		synchronized(this) {
-			time = System.currentTimeMillis();
-			while (time <= prev) {
-				try {
-					TimeUnit.MILLISECONDS.sleep(1);
-				} catch (InterruptedException e) {
-					// Ignore and go round again if needs be.
-				}
-				time = System.currentTimeMillis();
-			}
-			prev = time;
-		}
-		
-		
-		ZonedDateTime instant = ZonedDateTime.ofInstant(Instant.ofEpochMilli(time),ZoneOffset.UTC);
+    }
 
-		try {
-			return new RN(authority, instance, type, instant);
-		} catch (RNException e) {
-			return null;
-		}
-		
-	}
-	
-	/** @@TODO Factor this main out into some JUnit tests */
-	
-	public static void main(String[] args) {
-		// Make factory
-		RNFactory rnf = null;
-		RNFactory rnf2 = null;
-		
-		rnf =  RNFactory.getFactory(new Authority(1000), new Instance(1), new Type(21)) ;
-		rnf2 = RNFactory.getFactory(new Authority(1000), new Instance(1), new Type(21)) ;
-			
-		if(rnf.equals(rnf2) && rnf != rnf2) {
-			System.out.println("RNFs are strangely different");
-		}
-			
-		// Generate some RN's
-		RN [] arr = new RN[100];
-		for (int i = 0; i < 99; i++) {
-			arr[i] = rnf.generateReferenceNumber();
-		}
-		
-     	arr[99] = new RN( new BigInteger("999999999999991231235959"));
-	
-		// Print them out in various forms.
-		for(int i = 0; i<100; i++) {
-		   System.out.println( arr[i].getEncodedForm() + 
-				         " " + arr[i].getDecimalForm()+
-				         " " + arr[i].getDecodedDecimalForm() 
-		   );
-		}
-		
-		System.out.println();
-		
-		for(int i=0; i<100; i++) {
-			String ef = arr[i].getEncodedForm();
-			
-			// Break the encoded form
-			
-			ef = ef.replaceAll("K","P");
-			
-			RN rn;
-			try {
-				rn = new RN(Codec.decode(ef));
-				System.out.println(rn.getEncodedForm()+" "+rn.getDecimalForm()+" "+rn.getDecodedDecimalForm());			
-			} catch (RNException e) {
-			    System.out.println(e.getMessage());	
-			}
-		}
-	}
+    @Override
+    public int hashCode() {
+        int res = authority.getId();
+        res     = instance.getId() + res*(Instance.MAX_INSTANCE_ID+1);
+        res     = type.getId()     + res*(Type.MAX_TYPE_ID+1);
+        return res;
+    }
+
+    /*
+     *  Use a Map to ensure there is a single factory instance for each authority,instance and type combination
+     *  within the same JVM.
+     *
+     */
+
+    private static HashMap<Integer,RNFactory> factories = new HashMap<Integer,RNFactory>() ;
+
+    public static RNFactory getFactory(Authority authority, Instance instance, Type type) throws RNException {
+        RNFactory res = new RNFactory(authority, instance, type);
+
+        synchronized (factories) {
+            if (factories.containsKey(res.hashCode())) {
+                res = factories.get(res.hashCode());
+            } else {
+                factories.put(res.hashCode(), res);
+            }
+        }
+
+        return res;
+    }
+
+    private RNFactory(Authority authority, Instance instance, Type type) {
+        this.authority = authority;
+        this.instance  = instance;
+        this.type      = type;
+    }
+
+    public RN generateReferenceNumber() {
+        // Make sure at least one millisecond has elapse since the last reference number was generated
+        long time = 0;
+        synchronized(this) {
+            time = System.currentTimeMillis();
+            while (time <= prev) {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    // Ignore and go round again if needs be.
+                }
+                time = System.currentTimeMillis();
+            }
+            prev = time;
+        }
+
+
+        ZonedDateTime instant = ZonedDateTime.ofInstant(Instant.ofEpochMilli(time),ZoneOffset.UTC);
+
+        try {
+            return new RN(authority, instance, type, instant);
+        } catch (RNException e) {
+            return null;
+        }
+
+    }
+
+    /** @@TODO Factor this main out into some JUnit tests */
+
+    public static void main(String[] args) {
+//		// Make factory
+//		RNFactory rnf = null;
+//		RNFactory rnf2 = null;
+//
+//		rnf =  RNFactory.getFactory(new Authority(1000), new Instance(1), new Type(21)) ;
+//		rnf2 = RNFactory.getFactory(new Authority(1000), new Instance(1), new Type(21)) ;
+//
+//		if(rnf.equals(rnf2) && rnf != rnf2) {
+//			System.out.println("RNFs are strangely different");
+//		}
+//
+//		// Generate some RN's
+//		RN [] arr = new RN[100];
+//		for (int i = 0; i < 99; i++) {
+//			arr[i] = rnf.generateReferenceNumber();
+//		}
+//
+//     	arr[99] = new RN( new BigInteger("999999999999991231235959"));
+//
+//		// Print them out in various forms.
+//		for(int i = 0; i<100; i++) {
+//		   System.out.println( arr[i].getEncodedForm() +
+//				         " " + arr[i].getDecimalForm()+
+//				         " " + arr[i].getDecodedDecimalForm()
+//		   );
+//		}
+//
+//		System.out.println();
+//
+//		for(int i=0; i<100; i++) {
+//			String ef = arr[i].getEncodedForm();
+//
+//			// Break the encoded form
+//
+//			ef = ef.replaceAll("K","P");
+//
+//			RN rn;
+//			try {
+//				rn = new RN(Codec.decode(ef));
+//				System.out.println(rn.getEncodedForm()+" "+rn.getDecimalForm()+" "+rn.getDecodedDecimalForm());
+//			} catch (RNException e) {
+//			    System.out.println(e.getMessage());
+//			}
+//		}
+    }
 }
--- java-rn/src/test/java/uk/gov/food/rn/RNTest.java
+++ java-rn/src/test/java/uk/gov/food/rn/RNTest.java
@@ -1,15 +1,10 @@
-/*******************************************************************************
- * File:        RNTest.java
- * Created by:  Stuart Williams (skw@epimorphics.com)
- * Created on:  4 Apr 2018
- *
- * Copyright (c) 2018 Crown Copyright (Food Standards Agency)
- *
- ******************************************************************************/
+/* Copyright (c) 2018 Crown Copyright (Food Standards Agency) */
+
 package uk.gov.food.rn;
 
 import static org.junit.Assert.*;
 
+import java.math.BigInteger;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 
@@ -18,50 +13,93 @@ import org.junit.Test;
 import uk.gov.food.rn.Authority;
 import uk.gov.food.rn.Instance;
 import uk.gov.food.rn.RN;
-import uk.gov.food.rn.RNException;
-import uk.gov.food.rn.TimeStamp;
 import uk.gov.food.rn.Type;
 
 /**
- * Unit test for {@link RN}
+ * Unit tests for {@link RN}.
  */
-public class RNTest 
-{ 
-	@Test
-	public void decodeLastRN() {
-		RN rn;
-		try {
-			rn = new RN("SZVSDH-BQ8YGP-SJFWJ6");
-			assertTrue("authority id should be 9999", rn.getAuthority().equals(new Authority("9999")));
-			assertTrue("instance  id should be 9",    rn.getInstance().equals(new Instance("9")));
-			assertTrue("type id should be 99",        rn.getType().equals(new Type("99")));
-			assertTrue("timestamp should be 9999-12-31T23:59:59.999Z", 
-					rn.getInstant().equals(new TimeStamp(ZonedDateTime.of(9999, 12, 31,23,59,59,999*1000000,ZoneOffset.UTC))) );
-		} catch (Throwable e) {
-			fail("Unexpected Throwable: "+e.getMessage());
-		}	
-	}
-	
-	@Test 
-	public void detectCorruptRN() {
-		try {
-			RN rn = new RN("SZVSDH-BQ8YZP-SJFWJ6");
-			assertTrue("constructor should have fired excetion",rn==null);
-		} catch (RNException e) {
-			// Expected
-		}
-	}
-	
-	@Test
-	public void encodeRN() {
-		try {
-		assertTrue("", new RN(new Authority(1000),
-				              new Instance(1),
-				              new Type(21),
-				              ZonedDateTime.of(2018,04,12,12,34,51,468*1000000,ZoneOffset.UTC))
-						.getEncodedForm().equals("H41ZC8-20CPBR-0SDD9J"));
-		} catch (Throwable e) {
-			fail("Unexpected Throwable: "+ e.getMessage());
-		}
-	}
+public class RNTest
+{
+    @Test
+    public void itShouldConstructAnRNFromElements() {
+        RN rn = new RN(new Authority(1234), new Instance(5), new Type(6), timestampFixture());
+
+        assertEquals(1234, rn.getAuthority().getId());
+        assertEquals(5, rn.getInstance().getId());
+        assertEquals(6, rn.getType().getId());
+        assertEquals(timestampFixture(), rn.getInstant().getInstant());
+    }
+
+    @Test
+    public void itShouldConstructAnRNFromAnInternalNumber() {
+        RN rn = new RN(internalRepresentationFixture());
+
+        assertEquals(1234, rn.getAuthority().getId());
+        assertEquals(5, rn.getInstance().getId());
+        assertEquals(6, rn.getType().getId());
+        assertEquals(timestampFixture(), rn.getInstant().getInstant());
+    }
+
+    @Test
+    public void itShouldConstructAnRNFromAnEncodedNumber() {
+        RN rn = new RN("7SQ34N-8221TH-LMDNYG");
+
+        assertEquals(1234, rn.getAuthority().getId());
+        assertEquals(5, rn.getInstance().getId());
+        assertEquals(6, rn.getType().getId());
+        assertEquals(timestampFixture(), rn.getInstant().getInstant());
+    }
+
+    @Test
+    public void itShouldEncodeAReferenceNumberToExternalForm() {
+        RN rn = new RN(new Authority(1234), new Instance(5), new Type(6), timestampFixture());
+        assertEquals("7SQ34N-8221TH-LMDNYG", rn.getEncodedForm());
+    }
+
+    @Test
+    public void itShouldGenerateAHumanReadbleFormForDebugging() {
+        RN rn = new RN(new Authority(1234), new Instance(5), new Type(6), timestampFixture());
+        assertEquals("1234:5:06:2018-04-12T12:34:51.468Z", rn.toString());
+    }
+
+    @Test
+    public void itShouldDetectWhenAnExternalFormIsCorrupted() {
+        try {
+            String rnEnc = "7SQ34N-8221TH-LMDNYG";
+            String rnCorrupt = rnEnc.replace('Q', 'S');
+
+            new RN(rnCorrupt);
+            fail("Should raise exception");
+        }
+        catch (RNException e) {
+            assertEquals("Value '509906315616374199643526092' does not have intact check digits", e.getMessage());
+        }
+    }
+
+    @Test
+    public void itShouldDetectEquality() {
+        RN rn0 = new RN(new Authority(1234), new Instance(5), new Type(6), timestampFixture());
+        RN rn1 = new RN(new Authority(1234), new Instance(5), new Type(6), timestampFixture());
+
+        assertEquals(rn0, rn1);
+        assertEquals(rn0.hashCode(), rn1.hashCode());
+    }
+
+    @Test
+    public void itShouldDetectInequality() {
+        RN rn0 = new RN(new Authority(1234), new Instance(5), new Type(6), timestampFixture());
+        RN rn1 = new RN(new Authority(1243), new Instance(5), new Type(6), timestampFixture());
+
+        assertNotEquals(rn0, rn1);
+        assertNotEquals(rn0.hashCode(), rn1.hashCode());
+    }
+
+    protected ZonedDateTime timestampFixture() {
+        return ZonedDateTime.of(2018, 04, 12, 12, 34, 51, 468*1000000, ZoneOffset.UTC);
+    }
+
+    /** Corresponds to 1234:5:6:20180412123451 */
+    protected BigInteger internalRepresentationFixture() {
+        return new BigInteger("468123450620180412123451");
+    }
 }
