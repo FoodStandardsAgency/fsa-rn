3c4398aae124c8ddd9be1d4c62d771962972caf4
Max Prettyjohns <heshoots9999@gmail.com>
2018-08-21 10:14:21 +0100

    limit timestamp range to epoch seconds

--- java-rn/src/main/java/uk/gov/food/rn/TimeStamp.java
+++ java-rn/src/main/java/uk/gov/food/rn/TimeStamp.java
@@ -16,23 +16,24 @@ import java.time.ZonedDateTime;
  */
 public class TimeStamp  {
 	private ZonedDateTime instant;
-	
-	private static int MIN_INSTANT_YEAR = 2000 ;
-	private static int MAX_INSTANT_YEAR = 9999 ;
+
+	// Corresponds to 2000-01-01T00:00:00+00:00
+	private static long MIN_EPOCH_SECONDS = 946684800L ;
+	private static long MAX_EPOCH_SECONDS = 9999999999L ;
 	
 	public TimeStamp(ZonedDateTime instant) {
 		instant = instant.withZoneSameInstant(ZoneOffset.UTC);
 		if(!isValidInstant(instant)) {		
 			throw new RNException(String.format("Illegal instant year: %d is not in the range %d : %d",
-								                instant.getYear(), MIN_INSTANT_YEAR, MAX_INSTANT_YEAR)
+								                instant.toEpochSecond(), MIN_EPOCH_SECONDS, MAX_EPOCH_SECONDS)
 		    );
 		}
 		this.instant =  instant;
 	}
 
 	public static boolean isValidInstant(ZonedDateTime instant) {
-		int year = instant.getYear();
-		return MIN_INSTANT_YEAR <= year && year <= MAX_INSTANT_YEAR ;
+		long seconds = instant.toEpochSecond();
+		return MIN_EPOCH_SECONDS <= seconds && seconds <= MAX_EPOCH_SECONDS;
 	}
 	
 	/**
